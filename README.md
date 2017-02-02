882ae237934b17d0e0979b56f5638b4f91b5799d
Jordan Addison <jordanrileyaddison@gmail.com>
2017-02-01 23:34:56 -0600

    fixing readme

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ $ npm install -g two-things-bro
 |two-things-bro [options] |Description|
 |------------------------ |-----------|
 |`-t --task`| run 2 or more task at the same time of the command specified |
-|`-c --command`| The command to run on the task. Defaults to npm run`|
+|`-c --command`| The command to run on the task. Defaults to npm run |
 
-MIT LICENSE
+MIT License
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 const args = require('yargs').argv
 const lib = require('./lib')
-const debug = require('debug')('TwoThingsBro:cli')
 
 if (args.t || args.task) {
-  args.t ? lib.execArgs(args, 't').catch(err => debug(err)) : null
-  args.task ? lib.execArgs(args, 'task').catch(err => debug(err)) : null
+  args.t ? lib.execArgs(args, 't').catch(err => console.log(err)) : null
+  args.task ? lib.execArgs(args, 'task').catch(err => console.log(err)) : null
 }
